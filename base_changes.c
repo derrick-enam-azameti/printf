5e28f26101eb4f0e8b389e5be2e5ebbd9dda9377
Derrick Enam Azameti <azametid@gmail.com>
2022-08-09 18:26:45 -0700

    base_changes.c futher functions added

--- base_changes.c
+++ base_changes.c
@@ -130,3 +130,67 @@ int print_hex(va_list list)
 	free(rev_hex);
 	return (len);
 }
+
+/**
+ * print_heX - prints representation of a decimal number on base 16 uppercase
+ * @list: args
+ * Return: number of characters printed
+ */
+int print_heX(va_list list)
+{
+	unsinged int num;
+	int len;
+	int rem_num;
+	char *hex_rep;
+	char *rev_hex;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = hex_check(rem_num, 'X');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = rev_string(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	write_base(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
+}
+
+/**
+ * hex_check - checks which hex func is calling it
+ * @num: number to convert to letter
+ * @x: the hex func calling it
+ * Return: ASCII value for the letter
+ */
+int hex_check(int num, char x)
+{
+	char *hex = "abcdef";
+	char *Hex = "ABCDEF";
+
+	num = num - 10;
+	if (x == 'x')
+		return (hex[num]);
+	else
+		return (Hex[num]);
+	return (0);
+}
